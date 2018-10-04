e6bf3a036f8d5d939209764a3aa4fcc5321a9e4b
EC2 Default User <ec2-user@ip-172-31-0-138.us-east-2.compute.internal>
2018-10-04 05:23:12 +0000

    cucumber

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -16,8 +16,11 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/movies'
       
+    when /^the home page$/
+      '/'
+      
     when /^the RottenPotatoes home page$/
-      'movies'
+      '/movies'
       
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
